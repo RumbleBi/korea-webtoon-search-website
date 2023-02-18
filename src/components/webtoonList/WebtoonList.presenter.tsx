fbe29bbfc94b3efa166965b983e246460f4b1c10
RumbleBi <olzlel2000@gmail.com>
2023-02-18 17:31:44 +0900

    ADDING: 코드 정리

--- src/components/webtoonList/WebtoonList.presenter.tsx
+++ src/components/webtoonList/WebtoonList.presenter.tsx
@@ -5,8 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function WebtoonListUI({ webtoonList }: IWebtoonListUIProps): JSX.Element {
-  // https://image-comic.pstatic.net/webtoon/799862/thumbnail/thumbnail_IMAG21_fe93d28b-fc33-4a30-92d2-43e17c194491.jpg
-  // https://kr-a.kakaopagecdn.com/P/C/2727/c1/2x/9a0dafdc-3b44-4389-b70a-fdc3e241947f.png
   return (
     <Row>
       {webtoonList?.map((el, index) => (
@@ -19,14 +17,14 @@ export default function WebtoonListUI({ webtoonList }: IWebtoonListUIProps): JSX
               {el.additional?.adult && (
                 <Image src='/images/adult.png' alt='19' width={20} height={20} unoptimized={true} />
               )}
-
               <div>좋아요: {el.fanCount}만</div>
-              <div></div>
-              {el.url.includes("pstatic") ? (
-                <Image src={el.img} alt='img' width={150} height={150} />
-              ) : (
-                <img src={el.img} alt='img' width={150} height={150} />
-              )}
+              <S.WebtoonThumbnail>
+                {el.url.includes("pstatic") ? (
+                  <Image src={el.img} alt='img' width={150} height={200} />
+                ) : (
+                  <img src={el.img} alt='img' width={150} height={200} />
+                )}
+              </S.WebtoonThumbnail>
             </a>
           </S.WebtoonItem>
         </Col>
--- src/components/webtoonList/WebtoonList.styles.ts
+++ src/components/webtoonList/WebtoonList.styles.ts
@@ -5,8 +5,20 @@ export const WebtoonItem = styled.div`
   height: 50vh;
   background-color: green;
   border: 1px solid white;
-  padding: 2rem;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
+  a {
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    background-color: red;
+    align-items: center;
+  }
+`;
+export const WebtoonThumbnail = styled.div`
+  background-color: white;
+  width: inherit;
+  text-align: center;
 `;
